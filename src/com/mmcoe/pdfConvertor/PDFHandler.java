c7f6adb2107b8832ac452deb92a4b66009ce86b2
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-19 19:13:36 +0530

    Initial push
    pdfConvertor: Added Converting Text in pdf to simple text and give output to
    		the terminal

---
+++ src/com/mmcoe/pdfConvertor/PDFHandler.java
@@ -0,0 +1,15 @@
+package com.mmcoe.pdfConvertor;
+
+import java.io.IOException;
+
+import com.mmcoe.pdfConvertor.utils.Convertor;
+
+public class PDFHandler {
+
+	public static void main(String[] args) throws IOException {
+		Convertor C = new Convertor();
+		C.setFilePath(args[0]);
+		System.out.println(C.ToText());
+	}
+
+}
